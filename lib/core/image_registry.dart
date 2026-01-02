e5fc2ac63f5676b35ef015f944c53e825a2eded1
cyilmazcsa <153614119+cyilmazcsa@users.noreply.github.com>
2026-01-02 18:07:29 +0100

    Use hosted images instead of bundled assets

--- lib/core/image_registry.dart
+++ lib/core/image_registry.dart
@@ -1,39 +1,36 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:flutter/widgets.dart';
 
-/// Base64 encoded PNG placeholders for menu imagery to avoid binary assets.
-const Map<String, String> _menuImageBase64 = {
+const Map<String, String> _menuImageUrls = {
   'pizza_marg':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA7ElEQVR4nO3QAQkAIADAMPtjHJOYyBYKd/AAZ2OvqQuN5wefBBp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3eoAgVZ7Td7t2a8AAAAASUVORK5CYII=',
+      'https://upload.wikimedia.org/wikipedia/commons/4/4b/Margherita_Originale.jpg',
   'pizza_funghi':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA7ElEQVR4nO3QAQkAIADAMPtjHJOYyBYKd/AAZ2OvqQuN5wefBBp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3Qo06FagQbcCDboVaNCtQINuBRp0K9CgW4EG3eoAgVZ7Td7t2a8AAAAASUVORK5CYII=',
+      'https://upload.wikimedia.org/wikipedia/commons/1/1d/Pizza_funghi.jpg',
   'pizza_4cheese':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA60lEQVR4nO3QAQkAIADAMPsXsZsltIXCHTzA2dhr6kLj+cEngQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtzqw9Cxx7L93pQAAAABJRU5ErkJggg==',
+      'https://upload.wikimedia.org/wikipedia/commons/3/33/Pizza_quattro_formaggi.jpg',
   'pizza_pepperoni':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA7UlEQVR4nO3QAQkAIADAMGtYyf4ZzGELhTt4gLOx19SFxvODTwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag251AG1aTnlOR008AAAAAElFTkSuQmCC',
+      'https://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg',
   'salad_greek':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA60lEQVR4nO3QQQkAIADAQAP6sKkVbaEwDxZg3Jh76ULj+cEngQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtzo6R775jy008QAAAABJRU5ErkJggg==',
+      'https://upload.wikimedia.org/wikipedia/commons/9/9e/Greek_salad.jpg',
   'drink_cola':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA7UlEQVR4nO3QQQkAIADAQBP5Mof949hCYR4swLix19SFxvODTwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag251AETwrjuQTgE+AAAAAElFTkSuQmCC',
+      'https://upload.wikimedia.org/wikipedia/commons/5/5a/Coca-Cola_glass.jpg',
   'drink_fanta':
-      'iVBORw0KGgoAAAANSUhEUgAAAHgAAAB4CAIAAAC2BqGFAAAA6klEQVR4nO3QAQkAIADAMMtaxtLaQuEOHuBs7DV1ofH84JNAg24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbgQbdCjToVqBBtwINuhVo0K1Ag24FGnQr0KBbHcIBkdUEEKbqAAAAAElFTkSuQmCC',
+      'https://upload.wikimedia.org/wikipedia/commons/8/86/Fanta_Orange_%282019%29.jpg',
 };
 
-final Map<String, Uint8List> _decodedCache = {};
+const Map<String, String> _toppingImageUrls = {
+  'olives':
+      'https://upload.wikimedia.org/wikipedia/commons/1/1b/Green_olive_with_pit.jpg',
+};
 
-Uint8List menuImageBytes(String key) {
-  return _decodedCache.putIfAbsent(key, () {
-    final data = _menuImageBase64[key];
-    if (data == null) {
-      throw ArgumentError('Unknown menu image key: $key');
-    }
-    return base64Decode(data);
-  });
+String? toppingImageUrl(String id) => _toppingImageUrls[id];
+
+ImageProvider menuImageProvider(String key) {
+  final url = _menuImageUrls[key];
+  if (url == null) {
+    throw ArgumentError('Unknown menu image key: $key');
+  }
+  return NetworkImage(url);
 }
-
-ImageProvider menuImageProvider(String key) => MemoryImage(menuImageBytes(key));
 
 Image menuImageWidget(
   String key, {
